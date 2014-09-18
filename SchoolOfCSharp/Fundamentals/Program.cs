abeb1a208e6afebe118ebf5866527983a1e02e16
MartinChavez <martin.chavez@live.com>
2014-09-18 15:10:43 -0700

    Your first Type

--- SchoolOfCSharp/Fundamentals/Program.cs
+++ SchoolOfCSharp/Fundamentals/Program.cs
@@ -7,7 +7,9 @@ namespace Fundamentals
     {
         static void Main() //Method that will execute first when running the program
         {
-
+            //Every Object you work with, has a type, in this case, it is a System.Int type, which we use the keyword 'int' to represent it
+            int firstType = 3; //Your first Type
+            
             Console.WriteLine("Hello World"); //Your first statement, this will output to console 'Hello World'
             /*When writting Console Applications, you can use this statement to wait for User Input
              Then the CMD Window won't automatically close*/
