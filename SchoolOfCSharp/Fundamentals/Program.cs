a0e7057b97708ca123e33d08c44239eda24a7943
MartinChavez <martin.chavez@live.com>
2014-09-18 14:12:41 -0700

    Hello World

--- SchoolOfCSharp/Fundamentals/Program.cs
+++ SchoolOfCSharp/Fundamentals/Program.cs
@@ -3,11 +3,15 @@
 
 namespace Fundamentals
 {
-    class Program //Every songle time you want to execute code, it needs to be inside of a Type, in this case, a Class
+    class Program //Every single time you want to execute code, it needs to be inside of a Type, in this case, a Class
     {
-        static void Main(string[] args) //Method that will execute first when running the program
+        static void Main() //Method that will execute first when running the program
         {
-            //Content
+
+            Console.WriteLine("Hello World"); //Your first statement, this will output to console 'Hello World'
+            /*When writting Console Applications, you can use this statement to wait for User Input
+             Then the CMD Window won't automatically close*/
+            Console.ReadLine();
         }
     }
 }
