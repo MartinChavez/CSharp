46a2ee90f698a2ea469a274041c39d9d5914625a
MartinChavez <martin.chavez@live.com>
2014-09-25 15:28:43 -0700

    Interfaces

---
+++ SchoolOfCSharp/Arrays/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Arrays
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ArrayLenght()
+        {
+            int[] IntArray = new int[5];
+            const int expectedLenght = 5;
+
+            Assert.AreEqual(IntArray.Length, expectedLenght);
+        }
+
+        [TestMethod]
+        public void ArraysAreReferences()
+        {
+            int[] IntArray = new int[5]; // Int arrays are initialized on 0 by default
+            modifyArray(IntArray);
+            const int expectedVal = 1;
+                //Since Arrays are references, by entering to the method, the value of the reference is modified
+
+            Assert.AreEqual(IntArray[1], expectedVal);
+
+        }
+
+        private void modifyArray(int[] intArray)
+        {
+            intArray[1] = 1;
+        }
+
+        [TestMethod]
+        public void ArrayIndexOf()
+        {
+            int[] intArray = {1, 2, 3, 4, 5};
+
+            const int expectedVal = 2; //Since Arrays are zero based, we expect the integer 3 to be at position 2
+
+            Assert.AreEqual(Array.IndexOf(intArray, 3), expectedVal);
+
+        }
+    }
+}
---
+++ SchoolOfCSharp/InterfacesTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterfacesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InvokingMethodWithoutKnowingAboutObject()
+        {
+            IAnimal Tiger = new Tiger();
+            IAnimal Lion = new Lion();
+
+            List<IAnimal> animalList = new List<IAnimal>();
+            animalList.Add(Tiger);
+            animalList.Add(Lion);
+
+
+            foreach (var animal in animalList) //We can execute the methods of IAnimal without knowing the Object
+            {
+                animal.Breathe();
+            }
+
+        }
+    }
+}
