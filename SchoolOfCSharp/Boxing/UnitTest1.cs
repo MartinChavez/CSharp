935daa12199625242a4fb3ff47cb44af8d30dd8b
MartinChavez <martin.chavez@live.com>
2014-09-25 11:27:00 -0700

    Enum

--- SchoolOfCSharp/Boxing/UnitTest1.cs
+++ SchoolOfCSharp/Boxing/UnitTest1.cs
@@ -10,7 +10,17 @@ namespace Boxing
         public void Boxing()
         {
             int Integer = 5;
-            object Object = Integer;
+            object Object = Integer; //This is known as boxing, we create a reference of the integer value on the heap
+
+            Assert.IsInstanceOfType(Object, typeof(object));
+        }
+
+        public void UnBoxing()
+        {
+            Object integer = 5;
+            int Object = (int)integer; //This is known as unboxing, we create a local var with the value of Object
+
+            Assert.IsInstanceOfType(Object, typeof(object));
         }
     }
 }
---
+++ SchoolOfCSharp/Enum/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Enum
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public enum Animal  //Value type that is a set of named constants
+        {
+            Cat,
+            Dog,
+            Tiger,
+            Bear
+        }
+
+        [TestMethod]
+        public void EnumComparison()
+        {
+            Animal animal = Animal.Bear;
+
+            Assert.AreEqual(animal, Animal.Bear); //Less expensive than String comparison since it is an integer value
+            Assert.AreNotEqual(animal, Animal.Dog);
+        }
+    }
+}
