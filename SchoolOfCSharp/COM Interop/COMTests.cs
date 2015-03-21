59c7696f5496d2801aa8890705b4d75a54f37349
MartinChavez <martin.chavez@live.com>
2015-03-21 13:26:50 -0700

    Fixing COM Tests

--- SchoolOfCSharp/COM Interop/COMTests.cs
+++ SchoolOfCSharp/COM Interop/COMTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SKYPE4COMLib;
 
@@ -8,11 +8,12 @@ namespace COM_Interop
     public class ComTests
     {
         [TestMethod]
+        [ExpectedException(typeof(System.Runtime.InteropServices.COMException), "System.Runtime.InteropServices.COMException")] //It will throw if you don't have an skype Instance running
         public void SkypeComCalls()//This method will make COM calls to a running instance of Skype
         {
             var skypeClass = new Skype();//Note: On the running instance of Skype,you will get a message saying that another application wants to access Skype
-            var currentUser = skypeClass.CurrentUser;//You can interact with the object and execute methods and properties
-            Assert.AreEqual(currentUser.FullName,"Martin Chavez Aguilar");//My skype name is being retrieved at runtime
-        } 
+            var currentUser = skypeClass.CurrentUser;//You can interact with the COM object and execute methods and properties
+            Assert.IsNotNull(currentUser.FullName);//My skype name is being retrieved at runtime
+        }
     }
 }
