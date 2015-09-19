d359829da7313e3ec8e3c5af21c7e4058abe643d
MartinChavez <martin.chavez@live.com>
2015-09-19 11:15:27 -0700

    Updated class name

--- SchoolOfCSharp/LINQ/Linq.cs
+++ SchoolOfCSharp/LINQ/Linq.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LINQ
 {
     [TestClass]
-    public class UnitTest1
+    public class Linq
     {
         [TestMethod]
         public void TestMethod1()
